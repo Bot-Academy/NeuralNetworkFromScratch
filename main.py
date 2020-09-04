f2552f865d9672e7c33f5a95765c1ddef62bd7ba
Christian Coenen <coenen.christian@outlook.com>
2020-09-04 11:31:49 +0200

    Added working mnist implementation

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ import random
 import numpy as np
 from nn import NeuralNetwork
 
+
+def get_mnist():
+    with np.load('mnist.npz') as f:
+        images, labels = f['x_train'], f['y_train']
+    # Normalize training data
+    images = images.astype("float32") / 255
+    images = np.reshape(images, (images.shape[0], images.shape[1] * images.shape[2]))
+    # Labels -> One Hot encoding
+    print("Shape before one-hot encoding: ", labels.shape)
+    labels = np.eye(10)[labels]
+    print("Shape after one-hot encoding: ", labels.shape)
+    return images, labels
+
+
 random.seed(10)
-data = np.array([[0.3, 0.1, 0.6, 0.6, 0.35], [0.7, 0.9, 0.3, 0.65, 0.9]])
-labels = [0.5, 0.6]
-nn = NeuralNetwork(structure=[5, 5, 1], learning_rate=0.1)
-nn.train(data=data, labels=labels, epochs=50)
+# data = np.array([[0.3, 0.1, 0.6, 0.6, 0.35], [0.7, 0.9, 0.3, 0.65, 0.9]])
+# labels = [0.5, 0.6]
+data = get_mnist()
+nn = NeuralNetwork(layer_sizes=[784, 200, 10], activation_functions=['sigmoid', 'softmax'], learning_rate=0.001)
+nn.train(data=data[0], labels=data[1], batch_size=100, epochs=30)
--- nn.py
+++ nn.py
@@ -2,73 +2,102 @@ import numpy as np
 
 
 class NeuralNetwork:
-    def __init__(self, structure, learning_rate, activation_function='sigmoid', error_function='mean-squared-error'):
-        self.activation_function = activation_function
-        self.error_function = error_function
+    def __init__(self, layer_sizes, learning_rate, activation_functions, error_function='mean_squared_error'):
         self.learning_rate = learning_rate
 
-        self.layer = np.empty(len(structure), dtype=dict)
-        for idx in range(len(structure) - 1):
-            self.layer[idx] = {
-                'weights': np.random.random((structure[idx], structure[idx + 1])),
-                'bias': np.random.random((structure[idx + 1]))
-            }
+        self.layers = []
+        self.layers.append({
+            'num_neurons': layer_sizes[0],
+            'neuron_values': None,
+        })
+        for idx in range(1, len(layer_sizes)):
+            self.layers.append({
+                'num_neurons': layer_sizes[idx],
+                'pre_neuron_values': np.empty(layer_sizes[idx], dtype=list),
+                'neuron_values': np.empty(layer_sizes[idx], dtype=list),
+                'weights': np.random.random((layer_sizes[idx-1], layer_sizes[idx])) / layer_sizes[idx-1],
+                'bias': np.random.random((layer_sizes[idx])),
+                'activation': activation_functions[idx-1],
+            })
+            if idx == len(layer_sizes) - 1:
+                self.layers[idx]['error_function'] = error_function
 
-        self.pre_activated_neuron_values = np.empty(len(self.layer), dtype=list)
-        self.activated_neuron_values = np.empty(len(self.layer), dtype=list)
-
-    def forward_propagation(self, inputs):
-        for idx, layer in enumerate(self.layer):
-            if idx == 0:
-                self.pre_activated_neuron_values[idx] = inputs
-                self.activated_neuron_values[idx] = inputs
-            else:
-                self.pre_activated_neuron_values[idx] = self.layer[idx-1]['bias'] + \
-                                                        self.activated_neuron_values[idx-1] @ \
-                                                        self.layer[idx-1]['weights']
-                self.activated_neuron_values[idx] = self.apply_activation_function(self.pre_activated_neuron_values[idx])
-
-        return self.activated_neuron_values[-1]
-
-    def apply_activation_function(self, activation_inputs):
-        if self.activation_function == 'sigmoid':
-            return 1 / (1 + np.exp(-activation_inputs))
-        else:
-            print("Unknown activation function!")
-
-    def apply_activation_function_derivative(self, derivative_inputs):
-        if self.activation_function == 'sigmoid':
-            activations = self.apply_activation_function(derivative_inputs)
-            return activations * (1 - activations)
-        else:
-            print("Unknown activation function!")
-
-    def calculate_error(self, output, label):
-        if self.error_function == 'mean-squared-error':
-            return np.sum((output - label) ** 2)
-        else:
-            print("Unknown activation function!")
-
-    def backpropagation(self, error):
-        delta_output = np.array([error])
-        jacobian_output_weights = self.activated_neuron_values[-1] @ delta_output
-        jacobian_output_bias = np.sum(delta_output)
-        self.layer[-2]['weights'] += - self.learning_rate * jacobian_output_weights
-        self.layer[-2]['bias'] += - self.learning_rate * jacobian_output_bias
-
-        delta_hidden = np.transpose(
-            self.layer[-2]['weights']) * delta_output @ self.apply_activation_function_derivative(
-            self.pre_activated_neuron_values[-2])
-        jacobian_hidden_weights = self.activated_neuron_values[0] * delta_hidden
-        jacobian_hidden_bias = np.sum(delta_hidden)
-        self.layer[-3]['weights'] += - self.learning_rate * jacobian_hidden_weights
-        self.layer[-3]['bias'] += - self.learning_rate * jacobian_hidden_bias
-
-    def train(self, data, labels, epochs):
+    def train(self, data, labels, batch_size, epochs):
         for epoch in range(epochs):
             print(f'Epoch: {epoch}')
-            for sample in data:
-                output = self.forward_propagation(sample)
-                error = self.calculate_error(output, labels)
-                print(f'Error: {error}')
-                self.backpropagation(error)
+            for i in range(0, len(data), batch_size):
+                data_batch = data[i:i + batch_size]
+                label_batch = labels[i:i + batch_size]
+                self.layers = forward_propagation(self.layers, data_batch)
+                error = calculate_error(self.layers[-1], label_batch, batch_size)
+                # print(f'Error: {np.sum(error)}')
+                acc = np.sum(np.argmax(self.layers[-1]['neuron_values'], axis=1) == np.argmax(label_batch, axis=1)) / batch_size
+                print(f'Acc: {acc}')
+                self.layers = backpropagation(self.layers, error, label_batch, self.learning_rate)
+
+
+def forward_propagation(layers, inputs):
+    for idx, layer in enumerate(layers):
+        if idx == 0:
+            layers[idx]['neuron_values'] = inputs
+        else:
+            layers[idx]['pre_neuron_values'] = layers[idx]['bias'] + \
+                                               layers[idx-1]['neuron_values'] @ layers[idx]['weights']
+            layers[idx]['neuron_values'] = apply_activation_function(layer)
+
+    return layers
+
+
+def apply_activation_function(layer):
+    values = layer['pre_neuron_values']
+
+    if layer['activation'] == 'sigmoid':
+        return 1 / (1 + np.exp(-values))
+    elif layer['activation'] == 'softmax':
+        e_x = np.exp(values - np.transpose(np.array([np.max(values, axis=1)])))
+        return e_x / np.transpose(np.array([e_x.sum(axis=1)]))
+    else:
+        print("Unknown activation function!")
+
+
+def apply_activation_function_derivative(layer):
+    activations = apply_activation_function(layer)
+
+    if layer['activation'] == 'sigmoid':
+        return activations * (1 - activations)
+    elif layer['activation'] == 'softmax':
+        activations_vector = activations.reshape(activations.shape[0], 1)
+        activations_matrix = np.tile(activations_vector, activations.shape[0])
+        return np.diag(activations) - (activations_matrix * np.transpose(activations_matrix))
+
+    else:
+        print("Unknown activation function!")
+
+
+def calculate_error(last_layer, labels, batch_size):
+    if last_layer['error_function'] == 'mean_squared_error':
+        return (1 / batch_size) * np.sum((last_layer['neuron_values'] - labels) ** 2, axis=0)
+    else:
+        print("Unknown activation function!")
+
+
+def backpropagation(layers, error, labels, learning_rate):
+    last_delta = None
+    # to make simultaneous weight updates
+    layers_new = np.copy(layers)
+    for idx in range(len(layers) - 1, 0, -1):
+        if idx == len(layers) - 1:
+            delta = layers[idx]['neuron_values'] - labels
+            jacobian_output_weights = np.transpose(layers[idx-1]['neuron_values']) @ delta
+            jacobian_output_bias = np.sum(delta)
+            layers_new[idx]['weights'] = layers[idx]['weights'] - learning_rate * jacobian_output_weights
+            layers_new[idx]['bias'] = layers[idx]['bias'] - learning_rate * jacobian_output_bias
+        else:
+            delta = layers[idx+1]['weights'] @ np.transpose(last_delta) * np.transpose(apply_activation_function_derivative(layers[idx]))
+            jacobian_hidden_weights = np.transpose(layers[idx-1]['neuron_values']) @ np.transpose(delta)
+            jacobian_hidden_bias = np.sum(delta)
+            layers_new[idx]['weights'] = layers[idx]['weights'] - learning_rate * jacobian_hidden_weights
+            layers_new[idx]['bias'] = layers[idx]['bias'] - learning_rate * jacobian_hidden_bias
+
+        last_delta = delta
+    return layers_new
