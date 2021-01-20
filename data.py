e088b8440dd2cb6364fe3ab169d46c2835adef0e
Christian Coenen <coenen.christian@outlook.com>
2021-01-20 21:04:16 +0100

    Update nn code to reflect the video version

--- data.py
+++ data.py
@@ -1,8 +1,10 @@
 import numpy as np
+import pathlib
+
 
 def get_mnist():
-    with np.load('data/mnist.npz') as f:
-        images, labels = f['x_train'], f['y_train']
+    with np.load(f"{pathlib.Path(__file__).parent.absolute()}/data/mnist.npz") as f:
+        images, labels = f["x_train"], f["y_train"]
     images = images.astype("float32") / 255
     images = np.reshape(images, (images.shape[0], images.shape[1] * images.shape[2]))
     labels = np.eye(10)[labels]
--- nn.py
+++ nn.py
@@ -1,62 +1,62 @@
-import helper
+from data import get_mnist
 import numpy as np
 import matplotlib.pyplot as plt
 
-'''
+
+"""
 w = weights, b = bias, i = input, h = hidden, o = output, l = label
 e.g. w_i_h = weights from input layer to hidden layer
-'''
-data, labels = helper.get_mnist()
+"""
+images, labels = get_mnist()
 w_i_h = np.random.uniform(-0.5, 0.5, (20, 784))
 w_h_o = np.random.uniform(-0.5, 0.5, (10, 20))
 b_i_h = np.zeros((20, 1))
 b_h_o = np.zeros((10, 1))
 
 learn_rate = 0.01
+nr_correct = 0
 epochs = 3
-batch_size = 1
-acc = 0
 for epoch in range(epochs):
-    for i, l in zip(data, labels):
-        i.shape += (1,)
+    for img, l in zip(images, labels):
+        img.shape += (1,)
         l.shape += (1,)
         # Forward propagation input -> hidden
-        h_pre = b_i_h + w_i_h @ i
+        h_pre = b_i_h + w_i_h @ img
         h = 1 / (1 + np.exp(-h_pre))
         # Forward propagation hidden -> output
         o_pre = b_h_o + w_h_o @ h
         o = 1 / (1 + np.exp(-o_pre))
 
-        # Error calculation
-        e = np.sum((o - l) ** 2, axis=0)
-        is_correct = np.argmax(o) == np.argmax(l)
+        # Cost / Error calculation
+        e = 1 / len(o) * np.sum((o - l) ** 2, axis=0)
+        nr_correct += int(np.argmax(o) == np.argmax(l))
 
         # Backpropagation output -> hidden (cost function derivative)
         delta_o = o - l
         w_h_o += -learn_rate * delta_o @ np.transpose(h)
-        b_h_o += -learn_rate * np.sum(delta_o)
+        b_h_o += -learn_rate * delta_o
         # Backpropagation hidden -> input (activation function derivative)
         delta_h = np.transpose(w_h_o) @ delta_o * (h * (1 - h))
-        w_i_h += -learn_rate * delta_h @ np.transpose(i)
-        b_i_h += -learn_rate * np.sum(delta_h)
+        w_i_h += -learn_rate * delta_h @ np.transpose(img)
+        b_i_h += -learn_rate * delta_h
 
-        # Show accuracy for this epoch
-        acc += is_correct
-    print(f'Acc: {round((acc / data.shape[0]) * 100, 2)}%')
-    acc = 0
+    # Show accuracy for this epoch
+    print(f"Acc: {round((nr_correct / images.shape[0]) * 100, 2)}%")
+    nr_correct = 0
 
 # Show results
 while True:
-    image_number = int(input("Enter a number (0 - 59999): "))
-    image_index = image_number
-    plt.imshow(data[image_index].reshape(28, 28), cmap='Greys')
+    index = int(input("Enter a number (0 - 59999): "))
+    img = images[index]
+    plt.imshow(img.reshape(28, 28), cmap="Greys")
 
+    img.shape += (1,)
     # Forward propagation input -> hidden
-    h_pre = b_i_h + w_i_h @ data[image_index].reshape(784, 1)
+    h_pre = b_i_h + w_i_h @ img.reshape(784, 1)
     h = 1 / (1 + np.exp(-h_pre))
     # Forward propagation hidden -> output
     o_pre = b_h_o + w_h_o @ h
     o = 1 / (1 + np.exp(-o_pre))
 
-    plt.title(f'Subscribe if its a {o.argmax()} :)')
+    plt.title(f"Subscribe if its a {o.argmax()} :)")
     plt.show()
